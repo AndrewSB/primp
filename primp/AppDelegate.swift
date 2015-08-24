ce968e546c07cd153fae142a4be44944adb45064
Andrew Breckenridge <asbreckenridge@me.com>
2015-08-24 01:07:51 -0700

    init

---
+++ primp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  primp
+//
+//  Created by Andrew Breckenridge on 8/24/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ primp/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  primp
+//
+//  Created by Andrew Breckenridge on 8/24/15.
+//  Copyright © 2015 Andrew Breckenridge. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
